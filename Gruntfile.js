58774f6f00eeac02bed0fcd37b542915fe7f06bc
Kirill Sh <kshumov89@gmail.com>
2017-01-19 15:26:11 +0300

    First Add

---
+++ Gruntfile.js
@@ -0,0 +1,28 @@
+module.exports = function(grunt) {
+
+    grunt.initConfig({
+        sass: {
+            options: {
+                outputStyle: 'expanded'
+            },
+            dist: {
+                files: [{
+                    expand: true,
+                    cwd: 'asserts/sass/',
+                    src: '**/*.scss',
+                    dest: 'asserts/css',
+                    ext: '.css'
+                }]
+            }
+        },
+        watch: {
+            files: 'asserts/sass/**/*.scss',
+            tasks: 'sass'
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    grunt.registerTask('default', ['sass', 'watch']);
+};
